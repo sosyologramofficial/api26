b2d1b541595289cf8ada90c8c328463afed5acf2
sosyologramofficial <sosyologramofficial@gmail.com>
2026-07-29 18:56:13 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,7 +7,7 @@ bind = "0.0.0.0:10000"
 
 def post_fork(server, worker):
     import threading
-    from service import resume_incomplete_tasks
+    from api import resume_incomplete_tasks
     
     def startup():
         import time
